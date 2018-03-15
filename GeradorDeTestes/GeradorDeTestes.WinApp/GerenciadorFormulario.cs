f24df767f4a4e54e312e563e9f7dfd142493a806
Ana Tosetto <ndduser@ndd-not-ace913.nddigital.local>
2018-03-15 13:38:42 -0300

    Iniciando classes

---
+++ GeradorDeTestes/GeradorDeTestes.WinApp/GerenciadorFormulario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeradorDeTestes.WinApp
+{
+    public abstract class GerenciadorFormulario
+    {
+        public abstract void Adicionar();
+
+        public abstract void Excluir();
+
+        public abstract void CarregarListagem();
+    }
+}
