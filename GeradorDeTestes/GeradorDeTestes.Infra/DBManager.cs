33e74afc0f7ebf4da807d5a0eec8c420b453a57b
Leonardo Magalhaes <leonardo.magalhaes@ndd.com.br>
2018-03-16 15:29:17 -0300

    Feito refatoração na classe dbmanager

--- GeradorDeTestes/GeradorDeTestes.Infra/DBManager.cs
+++ GeradorDeTestes/GeradorDeTestes.Infra/DBManager.cs
@@ -1,7 +1,4 @@
-﻿
-
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -10,101 +7,87 @@ using System.Linq;
 
 namespace GeradorDeTestes.Infra
 {
-   
-        public class DBManager
+
+    public class DBManager
+    {
+        private static string _connectionString = ConfigurationManager.ConnectionStrings["GERADORDETESTE"].ConnectionString;
+
+        private static string _providerName = ConfigurationManager.AppSettings.Get("DataProvider");
+
+        private static DbProviderFactory _providerType = DbProviderFactories.GetFactory(_providerName);
+
+        public void Insert(string sql, Dictionary<string, object> dictionary)
         {
-
-            private static string _connectionString = ConfigurationManager.ConnectionStrings["GERADORDETESTE"].ConnectionString;
-
-            private static string _providerName = ConfigurationManager.AppSettings.Get("DataProvider");
-
-            private static DbProviderFactory _providerType = DbProviderFactories.GetFactory(_providerName);
-
-            private static DbCommand _commandGetAll;
-
-            public void Insert(string sql, Dictionary<string, object> dictionary)
-            {
-
-            InitializeConnection(sql, dictionary).Close();
-              
-            }
-
-            public void Update(string sql, Dictionary<string, object> dictionary)
-            {
-            InitializeConnection(sql, dictionary).Close();
-            }
-
-            public void Delete(string sql, Dictionary<string, object> dictionary)
-            {
-            InitializeConnection(sql, dictionary).Close();
-            }
-
-            public static void SetParameters(DbCommand command, Dictionary<string, object> dictionary)
-            {
-                if (dictionary != null)
-                {
-                    foreach (var item in dictionary)
-                    {
-                        var dbParameter = command.CreateParameter();
-                        dbParameter.ParameterName = item.Key;
-                        dbParameter.Value = item.Value;
-                        command.Parameters.Add(dbParameter);
-                    }
-                }
-            }
-
-            //ok
-            public IQueryable<T> GetAll<T>(String sql, Func<IDataReader, T> convertRelactionalData)
-            {
-
-            
-            var connectionaux= InitializeConnection(sql,null,true);
-
-            var reader = _commandGetAll.ExecuteReader();
-
-            var list = new List<T>();
-
-                while (reader.Read())
-                {
-                    var obj = convertRelactionalData(reader);
-                    list.Add(obj);
-                }
-
-             connectionaux.Close();
-
-                return list.AsQueryable();
-
-            }
-
-            // Connection SQL
-            public static DbConnection InitializeConnection(string sql, Dictionary<string, object> parms = null, bool getAllIsTrue = false)
-            {
-                    DbConnection connection = _providerType.CreateConnection();
-                
-                    connection.ConnectionString = _connectionString;
-
-                    using (DbCommand command = _providerType.CreateCommand())
-                    {
-
-                        command.Connection = connection;
-                        command.CommandText = sql;
-                        SetParameters(command, parms);
-                        connection.Open();
-
-                    if (getAllIsTrue == false)
-                     {
-                    command.ExecuteScalar();
-                     }else{
-                    _commandGetAll = command;
-                     }
-
-                return connection;
-
-                }
-
-                }
-              }
-
+            InitializeConnection(sql, dictionary);
         }
-    
+
+        public void Update(string sql, Dictionary<string, object> dictionary)
+        {
+            InitializeConnection(sql, dictionary);
+        }
+
+        public void Delete(string sql, Dictionary<string, object> dictionary)
+        {
+            InitializeConnection(sql, dictionary);
+        }
+
+        public static void SetParameters(DbCommand command, Dictionary<string, object> dictionary)
+        {
+            if (dictionary != null)
+            {
+                foreach (var item in dictionary)
+                {
+                    var dbParameter = command.CreateParameter();
+                    dbParameter.ParameterName = item.Key;
+                    dbParameter.Value = item.Value;
+                    command.Parameters.Add(dbParameter);
+                }
+            }
+        }
+
+        //ok
+        public IQueryable<T> GetAll<T>(String sql, Func<IDataReader, T> convertRelactionalData)
+        {
+            using (DbConnection connection = _providerType.CreateConnection())
+            {
+                connection.ConnectionString = _connectionString;
+
+                using (DbCommand command = _providerType.CreateCommand())
+                {
+                    var reader = command.ExecuteReader();
+
+                    var list = new List<T>();
+
+                    while (reader.Read())
+                    {
+                        var obj = convertRelactionalData(reader);
+                        list.Add(obj);
+                    }
+                    return list.AsQueryable();
+                }
+            }
+        }
+
+        // Connection SQL
+        public static void InitializeConnection(string sql, Dictionary<string, object> parms = null)
+        {
+            using (DbConnection connection = _providerType.CreateConnection())
+            {
+                connection.ConnectionString = _connectionString;
+                using (DbCommand command = _providerType.CreateCommand())
+                {
+
+                    command.Connection = connection;
+                    command.CommandText = sql;
+                    SetParameters(command, parms);
+                    connection.Open();
+
+                    command.ExecuteScalar();
+                }
+            }
+        }
+    }
+
+}
+
 
