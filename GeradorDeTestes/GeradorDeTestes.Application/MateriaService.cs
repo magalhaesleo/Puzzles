b6692ba32f197bcf87b6b2533ba63625cb720943
Leonardo Magalhaes <leonardo.magalhaes@ndd.com.br>
2018-03-27 16:12:19 -0300

    Adicionado classe de serviço da materia

---
+++ GeradorDeTestes/GeradorDeTestes.Application/MateriaService.cs
@@ -0,0 +1,72 @@
+﻿using GeradorDeTestes.Domain.Entidades;
+using GeradorDeTestes.Infra.Data;
+using System;
+using System.Collections.Generic;
+
+namespace GeradorDeTestes.Applications
+{
+    public class MateriaService
+    {
+        private MateriaDAO _materiaDAO;
+
+        public MateriaService()
+        {
+            _materiaDAO = new MateriaDAO();
+        }
+
+        public Materia AdicionarMateria(Materia materia)
+        {            
+            try
+            {
+                materia.Validate();
+                _materiaDAO.Add(materia);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            return materia;
+        }
+
+        public Materia AtualizarMateria(Materia materia)
+        {
+            try
+            {
+                materia.Validate();
+                _materiaDAO.Editar(materia);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+
+            return materia;
+        }
+
+        public Materia ExcluirMateria(Materia materia)
+        {
+            try
+            {
+                _materiaDAO.Excluir(materia);
+            }
+            catch(Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+
+            return materia;
+        }
+                
+       public List<Materia> SelecionarTodasMaterias()
+        {
+            try
+            {
+                return _materiaDAO.GetAll();
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+    }
+}
