2bb3679fbba89895d4aee8f0df8f346156cda362
William Passig de Souza <william.passig@nddigital.com.br>
2018-03-27 16:49:21 -0300

    retornando ao estado normal

---
+++ GeradorDeTestes/GeradorDeTestes.Domain/Entidades/Serie.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeradorDeTestes.Domain.Entidades
+{
+    public class Serie
+    {
+        private int _id;
+
+        private int _numero;
+
+
+        public int Id { get { return _id; } set { _id = value; } }
+
+        public int Numero { get { return _numero; } set { _numero = value; } }
+        public Serie()
+        {
+
+        }
+        public Serie(int numero)
+        {
+            this.Numero = numero;
+        }
+        public override string ToString()
+        {
+            return String.Format("{0}ª serie", Numero);
+        }
+    }
+}
