ecf2f3911bc93a50fa31e81e85e2ecfe5df4f2da
William Passig de Souza <william.passig@nddigital.com.br>
2018-03-29 18:06:03 -0300

    Criado um teste de disciplina

--- GeradorDeTestes/GeradorDeTestes.Tests/Entidades/DisciplinaTests.cs
+++ GeradorDeTestes/GeradorDeTestes.Tests/Entidades/DisciplinaTests.cs
@@ -27,5 +27,23 @@ namespace GeradorDeTestes.Domain.Entidades.Tests
             disciplina.Validate();
             Assert.Fail();
         }
+
+        [TestMethod()]
+        [ExpectedException(typeof(Exception), "O nome deve ter pelo menos quatro caracteres.")]
+        public void ValidarNomeMenorQueQuatroCaracteresCorreto()
+        {
+            disciplina.Nome = "abc";
+            disciplina.Validate();
+            Assert.Equals;
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(Exception), "O nome não deve possuir mais que um espaço consecutivos.")]
+        public void ValidarEspacosConsecutivos()
+        {
+            disciplina.Nome = "Ana  Raquel";
+            disciplina.Validate();
+            Assert.Fail();
+        }
     }
 }
