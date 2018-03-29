ca5b7768beab33b221412f2f6286edac23fd81df
William Passig de Souza <william.passig@nddigital.com.br>
2018-03-29 17:01:08 -0300

    Criados o esqueleto dos três testes de domínio

---
+++ GeradorDeTestes/GeradorDeTestes.Tests/Entidades/DisciplinaTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GeradorDeTestes.Domain.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeradorDeTestes.Domain.Entidades.Tests
+{
+    [TestClass()]
+    public class DisciplinaTests
+    {
+        Disciplina disciplina;
+
+        [TestInitialize()]
+        public void Initialize()
+        {
+            disciplina = new Disciplina();
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(Exception), "O nome deve ter pelo menos quatro caracteres.")]
+        public void ValidarNomeMenorQueQuatroCaracteres()
+        {
+            disciplina.Nome = "abc";
+            disciplina.Validate();
+            Assert.Fail();
+        }
+    }
+}
