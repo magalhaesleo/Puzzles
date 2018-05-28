4c4c8380ff5f79c6d06d0b276efea9c6e31dd335
joao.azevedo <joao.azevedo@nddigital.com.br>
2018-05-28 16:37:49 -0300

    Foram adicionadas as propriedades da classe 'Endereco' e criada a classe 'Entidade'

--- Projeto_NFe/Projeto_NFe.Domain/Entidades/Enderecos/Endereco.cs
+++ Projeto_NFe/Projeto_NFe.Domain/Entidades/Enderecos/Endereco.cs
@@ -6,8 +6,17 @@ using System.Threading.Tasks;
 
 namespace Projeto_NFe.Domain.Entidades.Enderecos
 {
-    public class Endereco
+    public class Endereco : Entidade
     {
+        public string Logradouro { get; set; }
+        public int Numero { get; set; }
+        public string Bairro { get; set; }
+        public string Municipio { get; set; }
+        public string Estado { get; set; }
+        public string Pais { get; set; }
+        public void Validar()
+        {
 
+        }
     }
 }
