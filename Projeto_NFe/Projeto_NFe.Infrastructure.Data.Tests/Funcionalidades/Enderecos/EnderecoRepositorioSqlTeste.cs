54a5cafe757f3dbe7a5c4057cf84c96a6b5882cb
joao.azevedo <joao.azevedo@nddigital.com.br>
2018-05-29 14:18:30 -0300

    Foi adicionada a classe de teste para a classe 'EnderecoRepositorioSql'

---
+++ Projeto_NFe/Projeto_NFe.Infrastructure.Data.Tests/Funcionalidades/Enderecos/EnderecoRepositorioSqlTeste.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_NFe.Infrastructure.Data.Tests.Funcionalidades.Enderecos
+{
+    [TestFixture]
+    public class EnderecoRepositorioSqlTeste
+    {
+    }
+}
