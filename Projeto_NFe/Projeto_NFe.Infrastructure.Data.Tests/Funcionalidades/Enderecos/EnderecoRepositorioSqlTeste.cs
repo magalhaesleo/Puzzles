5b200fc3875a1905011f4917e1974cba08950525
joao.azevedo <joao.azevedo@nddigital.com.br>
2018-05-29 15:18:32 -0300

    Iniciados alguns testes de repositório referentes a entidade 'Endereco'

--- Projeto_NFe/Projeto_NFe.Infrastructure.Data.Tests/Funcionalidades/Enderecos/EnderecoRepositorioSqlTeste.cs
+++ Projeto_NFe/Projeto_NFe.Infrastructure.Data.Tests/Funcionalidades/Enderecos/EnderecoRepositorioSqlTeste.cs
@@ -1,4 +1,9 @@
-﻿using NUnit.Framework;
+﻿using FluentAssertions;
+using NUnit.Framework;
+using Projeto_NFe.Common.Tests.Base;
+using Projeto_NFe.Common.Tests.Funcionalidades.Enderecos;
+using Projeto_NFe.Domain.Funcionalidades.Enderecos;
+using Projeto_NFe.Infrastructure.Data.Funcionalidades;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,8 +16,45 @@ namespace Projeto_NFe.Infrastructure.Data.Tests.Funcionalidades.Enderecos
     public class EnderecoRepositorioSqlTeste
     {
 
-        //[SetUp]
+        EnderecoRepositorioSql _repositorio;
+
+        [SetUp]
+        public void IniciarCenario()
+        {
+            _repositorio = new EnderecoRepositorioSql();
+
+            BaseSqlTeste.InicializarBancoDeDados();
+        }
+
+        [Test]
+        public void EnderecoRepositorioSql_Adicionar_Sucesso()
+        {
+            Endereco endereco = ObjectMother.PegarEnderecoValido();
+            endereco.Id = 0;
+
+            Endereco resultado = _repositorio.Adicionar(endereco);
+
+            resultado.Id.Should().BeGreaterThan(0);
+        }
+
+        [Test]
+        public void EnderecoRepositorioSql_Atualizar_Sucesso()
+        {
+            Endereco endereco = ObjectMother.PegarEnderecoValido();
+            endereco.Pais = "Brasil";
+            endereco.Id = 1;
+
+            Endereco resultado = _repositorio.Atualizar(endereco);
+
+            resultado.Pais.Should().Be("Brasil");
+            resultado.Pais.Should().NotBe(ObjectMother.PegarEnderecoValido().Pais);
+            resultado.Id.Should().Be(endereco.Id);
+        }
         
+        public void EnderecoRepositorioSql_Excluir_Sucesso()
+        {
+
+        }
 
     }
 }
--- Projeto_NFe/Projeto_NFe.Infrastructure.Data/Funcionalidades/EnderecoRepositorioSql.cs
+++ Projeto_NFe/Projeto_NFe.Infrastructure.Data/Funcionalidades/EnderecoRepositorioSql.cs
@@ -1,5 +1,7 @@
-﻿using Projeto_NFe.Domain.Funcionalidades.Enderecos;
+﻿using Projeto_NFe.Domain.Excecoes;
+using Projeto_NFe.Domain.Funcionalidades.Enderecos;
 using Projeto_NFe.Domain.Interfaces;
+using Projeto_NFe.Infrastructure.Database;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,31 +10,75 @@ using System.Threading.Tasks;
 
 namespace Projeto_NFe.Infrastructure.Data.Funcionalidades
 {
-    public class EnderecoRepositorioSql : IRepositorio<Endereco>
+    public class EnderecoRepositorioSql : IEnderecoRepositorio
     {
-        public Endereco Adicionar(Endereco entidade)
+        public Endereco Adicionar(Endereco endereco)
+        {
+            if (endereco.Id == 0)
+            {
+                endereco.Id = Db.Adicionar(endereco);
+                return endereco;
+            }
+            else
+                throw new ExcecaoIdentificadorIndefinido();
+
+        }
+
+        public Endereco Atualizar(Endereco endereco)
         {
             throw new NotImplementedException();
         }
 
-        public Endereco Editar(Endereco entidade)
+        public Endereco BuscarPorId(long Id)
         {
             throw new NotImplementedException();
         }
 
-        public Endereco ObterPorId(long id)
+        public IEnumerable<Endereco> BuscarTodos()
         {
             throw new NotImplementedException();
         }
 
-        public IEnumerable<Endereco> ObterTodos()
+        public void Excluir(Endereco endereco)
         {
             throw new NotImplementedException();
         }
 
-        public void Remover(Endereco entidade)
-        {
-            throw new NotImplementedException();
-        }
+        #region montar e ler objetos
+        //private Dictionary<string, object> RetornaDictionaryDeAlternativa(Alternativa alternativa)
+        //{
+        //    return new Dictionary<string, object>
+        //    {
+        //        {"ID", alternativa.Id },
+        //        { "ENUNCIADO", alternativa.Enunciado},
+        //        { "CORRETA", alternativa.Correta },
+        //        { "IDQUESTAO", alternativa.IdQuestao },
+        //        { "LETRA", alternativa.Letra},
+        //    };
+        //}
+
+        //Dictionary<string, object> IAlternativaRepository.RetornaDictionaryDeAlternativa(Alternativa alternativa)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //Func<IDataReader, Alternativa> IAlternativaRepository.FormaObjetoAlternativa(IDataReader reader)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //private static Func<IDataReader, Alternativa> FormaObjetoAlternativa = reader =>
+
+        //    new Alternativa
+        //    {
+        //        Id = Convert.ToInt32(reader["Id"]),
+        //        Enunciado = Convert.ToString(reader["ENUNCIADO"]),
+        //        Correta = Convert.ToBoolean(reader["CORRETA"]),
+        //        IdQuestao = Convert.ToInt32(reader["IDQUESTAO"]),
+        //        Letra = Convert.ToChar(reader["LETRA"])
+        //    };
+
+        #endregion
+
     }
 }
