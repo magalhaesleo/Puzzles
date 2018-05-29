75dc93740bc26fd6bf2b27f2af609f92f03dfb01
Ana <joao.azevedo@nddigital.com.br>
2018-05-29 15:02:50 -0300

    Adicionado a classe emitente ao dominio

---
+++ Projeto_NFe/Projeto_NFe.Common.Tests/Funcionalidades/Emitentes/ObjectMother.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_NFe.Common.Tests.Funcionalidades.Emitentes
+{
+    public static partial class ObjectMother
+    {
+        public static Emitente PegarEmitenteValido()
+        {
+            return new Emitente
+            {
+
+            }
+        }
+
+    }
+}
---
+++ Projeto_NFe/Projeto_NFe.Domain.Tests/Funcionalidades/Emitentes/EmitenteTeste.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_NFe.Domain.Tests.Funcionalidades.Emitentes
+{
+    [TestFixture]
+    public class EmitenteTeste
+    {
+        [SetUp]
+        public void Inicializa()
+        {
+
+        }
+        [Test]
+        public void Emitente_Validar_Sucesso()
+        {
+
+        }
+    }
+}
