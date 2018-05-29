5f1e871039c51bc7bc0be2d203dd14d76752a476
Ana <joao.azevedo@nddigital.com.br>
2018-05-29 17:59:19 -0300

    Criado o teste valido para CNPJ

---
+++ Projeto_NFe/Projeto_NFe.Infrastructure.Tests/Objetos de Valor/CNPJTeste.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using Projeto_NFe.Infrastructure.Objetos_de_Valor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_NFe.Infrastructure.Tests.Objetos_de_Valor
+{
+    [TestFixture]
+    public class CNPJTeste
+    {
+        [Test]
+        public void CNPJ_Validar_Sucesso()
+        {
+            CNPJ cnpj = new CNPJ();
+            cnpj.Numero = "56956041000100";
+
+            Action resultado = () => cnpj.Validar();
+
+            resultado.Should().NotThrow<Exception>();
+        }
+    }
+}
---
+++ Projeto_NFe/Projeto_NFe.Infrastructure/Objetos de Valor/CNPJ.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_NFe.Infrastructure.Objetos_de_Valor
+{
+    public class CNPJ
+    {
+        public string Numero { get; set; }
+        public void Validar()
+        {
+            int[] multiplicador1 = new int[12] { 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int[] multiplicador2 = new int[13] { 6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int soma;
+            int resto;
+            string digito;
+            string tempCnpj;
+            Numero = Numero.Trim();
+            Numero = Numero.Replace(".", "").Replace("-", "").Replace("/", "");
+
+            if (Numero.Length != 14)
+                throw new Exception("CNPJ deve possuir 14 caracteres");
+
+            tempCnpj = Numero.Substring(0, 12);
+            soma = 0;
+            for (int i = 0; i < 12; i++)
+                soma += int.Parse(tempCnpj[i].ToString()) * multiplicador1[i];
+            resto = (soma % 11);
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+            digito = resto.ToString();
+            tempCnpj = tempCnpj + digito;
+            soma = 0;
+            for (int i = 0; i < 13; i++)
+                soma += int.Parse(tempCnpj[i].ToString()) * multiplicador2[i];
+            resto = (soma % 11);
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+            digito = digito + resto.ToString();
+
+            if (Numero.EndsWith(digito) == false)
+                throw new Exception("CNPJ Invalido!");
+        }
+    }
+}
