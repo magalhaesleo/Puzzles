ec08c42fc469e04bb986bf591414d600402762ae
joao.azevedo <joao.azevedo@nddigital.com.br>
2018-05-29 13:50:43 -0300

    Foi criada a tabela da entidade 'Endereco' no projeto Database da aplicação e também foi implementado a interface de repositório que poderá ser utilizada por classes que sejam do tipo 'Entidade'

---
+++ Projeto_NFe/Projeto_NFe.Domain/Interfaces/IRepositorio.cs
@@ -0,0 +1,23 @@
+﻿using Projeto_NFe.Domain.Funcionalidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_NFe.Domain.Interfaces
+{
+    public interface IRepositorio<T> where T : Entidade
+    {
+        T Adicionar(T entidade);
+        T Editar(T entidade);
+        void Remover(T entidade);
+        T ObterPorId (long id);
+        IEnumerable<T> ObterTodos();
+    }
+}
+
+
+
+
+
