aee2c77a4fbdc0b9d4d27e24c46edc8f581fd08c
Ana <joao.azevedo@nddigital.com.br>
2018-06-07 17:55:03 -0300

    Criado a classe de dominio de nota fiscal e criado dois testes de validação

---
+++ Projeto_NFe/Projeto_NFe.Domain.Tests/Funcionalidades/Nota Fiscal/NotaFiscalTeste.cs
@@ -0,0 +1,42 @@
+﻿using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using Projeto_NFe.Domain.Excecoes;
+using Projeto_NFe.Domain.Funcionalidades.Nota_Fiscal;
+using Projeto_NFe.Domain.Funcionalidades.Transportadoras;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_NFe.Domain.Tests.Funcionalidades.Nota_Fiscal
+{
+    [TestFixture]
+    public class NotaFiscalTeste
+    {
+        Mock<Transportador> transportadorMock;
+        NotaFiscal _notaFiscal;
+        [SetUp]
+        public void IniciarCenario()
+        {
+            _notaFiscal = new NotaFiscal();
+            transportadorMock = new Mock<Transportador>();
+        }
+
+        [Test]
+        public void NotaFiscal_Validar_Sucesso()
+        {
+            _notaFiscal.Transportador = transportadorMock.Object;
+            Action resultado = _notaFiscal.Validar;
+            resultado.Should().NotThrow<ExcecaoDeNegocio>();
+        }
+
+        [Test]
+        public void NotaFiscal_Validar_SemTransportador_Falha()
+        {
+            Action resultado = _notaFiscal.Validar;
+            resultado.Should().Throw<ExcecaoTransportadorInvalido>();
+        }
+    }
+}
---
+++ Projeto_NFe/Projeto_NFe.Domain/Funcionalidades/Nota Fiscal/NotaFiscal.cs
@@ -0,0 +1,27 @@
+﻿using Projeto_NFe.Domain.Base;
+using Projeto_NFe.Domain.Funcionalidades.Destinatarios;
+using Projeto_NFe.Domain.Funcionalidades.Transportadoras;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_NFe.Domain.Funcionalidades.Nota_Fiscal
+{
+    public class NotaFiscal : Entidade
+    {
+        public Transportador Transportador { get; set; }
+        public Destinatario Destinatario { get; set; }
+        public string NaturezaOperacao { get; set; }
+        public DateTime DataEntrada { get; set; }
+        public void GerarCodigoDeAcesso()
+        {
+        }
+        public void Validar()
+        {
+            if (Transportador == null)
+                throw new ExcecaoTransportadorInvalido();
+        }
+    }
+}
