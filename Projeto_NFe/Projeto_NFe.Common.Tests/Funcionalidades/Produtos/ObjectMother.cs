27bd2449a699a4de99375db9f754309865f66f10
joao.azevedo <joao.azevedo@nddigital.com.br>
2018-06-07 15:33:20 -0300

    Feitos os testes e a respectiva implementação da classe Produto

---
+++ Projeto_NFe/Projeto_NFe.Common.Tests/Funcionalidades/Produtos/ObjectMother.cs
@@ -0,0 +1,49 @@
+﻿using Projeto_NFe.Domain.Funcionalidades.Produto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_NFe.Common.Tests.Funcionalidades.Produtos
+{
+    public static partial class ObjectMother
+    {
+        public static Produto ObterProdutoValido()
+        {
+            return new Produto()
+            {
+                Codigo = "12345",
+                Descricao = "Produto",
+                Valor = 0.89
+            };
+        }
+        public static Produto ObterProdutoComValorNegativo()
+        {
+            return new Produto()
+            {
+                Codigo = "12345",
+                Descricao = "Produto",
+                Valor = -2
+            };
+        }
+        public static Produto ObterProdutoSemCodigo()
+        {
+            return new Produto()
+            {
+                Codigo = "",
+                Descricao = "Produto",
+                Valor = 1
+            };
+        }
+        public static Produto ObterProdutoSemDescricao()
+        {
+            return new Produto()
+            {
+                Codigo = "12345",
+                Descricao = "",
+                Valor = 1
+            };
+        }
+    }
+}
---
+++ Projeto_NFe/Projeto_NFe.Domain/Funcionalidades/Produto/Excecoes/ExcecaoProdutoSemDescricao.cs
@@ -0,0 +1,16 @@
+﻿using Projeto_NFe.Domain.Excecoes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_NFe.Domain.Funcionalidades.Produto.Excecoes
+{
+    public class ExcecaoProdutoSemDescricao : ExcecaoDeNegocio
+    {
+        public ExcecaoProdutoSemDescricao() : base("O Produto deve conter uma descrição")
+        {
+        }
+    }
+}
