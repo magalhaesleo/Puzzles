1e2c84d0dc5f22def722e8d9337a67abf9ea2c4c
joao.azevedo <joao.azevedo@nddigital.com.br>
2018-06-07 15:40:12 -0300

    Preparando branch para o merge ao comentar detalhes ainda não implementados da classe ProdutoNotaFiscal

--- Projeto_NFe/Projeto_NFe.Domain/Funcionalidades/Produto/ProdutoNotaFiscal.cs
+++ Projeto_NFe/Projeto_NFe.Domain/Funcionalidades/Produto/ProdutoNotaFiscal.cs
@@ -8,17 +8,17 @@ namespace Projeto_NFe.Domain.Funcionalidades.Produto
 {
     public class ProdutoNotaFiscal
     {
-        public Produto Produto { get; set; }
+        //public Produto Produto { get; set; }
 
-        public NotaFiscal NotaFiscal { get; set; }
+        //public NotaFiscal NotaFiscal { get; set; }
 
-        public int Quantidade { get; set; }
+        //public int Quantidade { get; set; }
 
-        public ProdutoNotaFiscal(NotaFiscal notaFiscal, Produto produto, int quantidadeProduto)
-        {
-            NotaFiscal = notaFiscal;
-            Produto = produto;
-            Quantidade = quantidadeProduto;
-        }
+        //public ProdutoNotaFiscal(NotaFiscal notaFiscal, Produto produto, int quantidadeProduto)
+        //{
+        //    NotaFiscal = notaFiscal;
+        //    Produto = produto;
+        //    Quantidade = quantidadeProduto;
+        //}
     }
 }
