8b684b193c031ad4be29c0468ac33c596a55f67d
Ana <joao.azevedo@nddigital.com.br>
2018-06-08 13:58:08 -0300

    Criado os testes e implementado a entidade nota fiscal

--- Projeto_NFe/Projeto_NFe.Domain/Funcionalidades/Nota Fiscal/Excecoes/ExcecaoTransportadorInvalido.cs
+++ Projeto_NFe/Projeto_NFe.Domain/Funcionalidades/Nota Fiscal/Excecoes/ExcecaoTransportadorInvalido.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Projeto_NFe.Domain.Funcionalidades.Nota_Fiscal
+namespace Projeto_NFe.Domain.Funcionalidades.Nota_Fiscal.Excecoes
 {
     public class ExcecaoTransportadorInvalido : ExcecaoDeNegocio
     {
--- Projeto_NFe/Projeto_NFe.Domain/Funcionalidades/Nota Fiscal/NotaFiscal.cs
+++ Projeto_NFe/Projeto_NFe.Domain/Funcionalidades/Nota Fiscal/NotaFiscal.cs
@@ -1,5 +1,6 @@
 ﻿using Projeto_NFe.Domain.Base;
 using Projeto_NFe.Domain.Funcionalidades.Destinatarios;
+using Projeto_NFe.Domain.Funcionalidades.Nota_Fiscal.Excecoes;
 using Projeto_NFe.Domain.Funcionalidades.Transportadoras;
 using System;
 using System.Collections.Generic;
@@ -15,13 +16,23 @@ namespace Projeto_NFe.Domain.Funcionalidades.Nota_Fiscal
         public Destinatario Destinatario { get; set; }
         public string NaturezaOperacao { get; set; }
         public DateTime DataEntrada { get; set; }
-        public void GerarCodigoDeAcesso()
+        public string ChaveAcesso { get; set; }
+        public void GerarChaveDeAcesso()
         {
         }
         public void Validar()
         {
             if (Transportador == null)
                 throw new ExcecaoTransportadorInvalido();
+
+            if (Destinatario == null)
+                throw new ExcecaoDestinatarioInvalido();
+
+            if (string.IsNullOrEmpty(NaturezaOperacao))
+                throw new ExcecaoSemNaturezaOperacao();
+
+            if (DataEntrada > DateTime.Now)
+                throw new ExcecaoDataEntradaInvalida();
         }
     }
 }
