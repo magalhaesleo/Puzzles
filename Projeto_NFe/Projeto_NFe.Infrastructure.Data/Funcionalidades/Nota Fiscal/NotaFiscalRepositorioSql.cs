2aea374260374b29ba27e6a72945a157953b7ed7
joao.azevedo <joao.azevedo@nddigital.com.br>
2018-06-12 16:49:13 -0300

    Salvo progresso (não terminado) na classe NotaFiscalRepositorioSql

--- Projeto_NFe/Projeto_NFe.Infrastructure.Data/Funcionalidades/Nota Fiscal/NotaFiscalRepositorioSql.cs
+++ Projeto_NFe/Projeto_NFe.Infrastructure.Data/Funcionalidades/Nota Fiscal/NotaFiscalRepositorioSql.cs
@@ -7,6 +7,39 @@ using System.Threading.Tasks;
 
 namespace Projeto_NFe.Infrastructure.Data.Funcionalidades.Nota_Fiscal
 {
+    #region Scripts SQL
+
+    public const string _sqlAdicionar = @"";
+
+    public const string _sqlAtualizar = @"";
+
+    public const string _sqlBuscarPorId = @"
+
+ID = {0}ID";
+
+    public const string _sqlExcluir = @"DELETE FROM TBEMITENTE
+                                              WHERE ID = {0}ID";
+
+    public const string _sqlBuscarTodos = @"SELECT
+                                                TBEMITENTE.ID[IDEMITENTE],
+                                                TBEMITENTE.NOMEFANTASIA[NOME_FANTASIA],
+                                                TBEMITENTE.RAZAOSOCIAL[RAZAO_SOCIAL],
+                                                TBEMITENTE.CNPJ[CNPJ_EMITENTE],
+                                                TBEMITENTE.INSCRICAOESTADUAL[INSCRICAO_ESTADUAL],
+                                                TBEMITENTE.INSCRICAOMUNICIPAL[INSCRICAO_MUNICIPAL],
+                                                TBEMITENTE.ENDERECOID[ENDERECO_ID],
+                                                TBENDERECO.ID[IDENDERECO],
+                                                TBENDERECO.LOGRADOURO[LOGRADOURO_ENDERECO],
+                                                TBENDERECO.NUMERO[NUMERO_ENDERECO],
+                                                TBENDERECO.BAIRRO[BAIRRO_ENDERECO],
+                                                TBENDERECO.MUNICIPIO[MUNICIPIO_ENDERECO],
+                                                TBENDERECO.ESTADO[ESTADO_ENDERECO],
+                                                TBENDERECO.PAIS[PAIS_ENDERECO]
+                                                FROM TBEMITENTE
+                                                JOIN TBENDERECO ON TBEMITENTE.ENDERECOID = TBENDERECO.ID";
+
+    #endregion Scripts SQL
+
     public class NotaFiscalRepositorioSql : INotaFiscalRepositorio
     {
         public NotaFiscal Adicionar(NotaFiscal entidade)
