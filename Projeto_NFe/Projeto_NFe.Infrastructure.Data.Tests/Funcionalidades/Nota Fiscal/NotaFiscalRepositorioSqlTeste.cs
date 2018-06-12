922025531927ec60b81db8c2a4682a6eb00805d6
joao.azevedo <joao.azevedo@nddigital.com.br>
2018-06-12 16:48:32 -0300

    Iniciando repositório de nota fiscal (não emitida)

---
+++ Projeto_NFe/Projeto_NFe.Infrastructure.Data.Tests/Funcionalidades/Nota Fiscal/NotaFiscalRepositorioSqlTeste.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+using Projeto_NFe.Domain.Funcionalidades.Nota_Fiscal;
+using Projeto_NFe.Infrastructure.Data.Funcionalidades.Nota_Fiscal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_NFe.Infrastructure.Data.Tests.Funcionalidades.Nota_Fiscal
+{
+    [TestFixture]
+    public class NotaFiscalRepositorioSqlTeste
+    {
+        private INotaFiscalRepositorio _repositorio;
+
+        [SetUp]
+        public void IniciarCenario()
+        {
+            _repositorio = new NotaFiscalRepositorioSql();
+        }
+
+        [Test]
+        public void NotaFiscal_InfraData_Adicionar_Sucesso()
+        {
+            
+        }
+
+        [Test]
+        public void NotaFiscal_InfraData_Atualizar_Sucesso()
+        {
+        }
+
+        [Test]
+        public void NotaFiscal_InfraData_BuscarPorId_Sucesso()
+        {
+        }
+
+        [Test]
+        public void NotaFiscal_InfraData_BuscarTodos_Sucesso()
+        {
+        }
+
+        [Test]
+        public void NotaFiscal_InfraData_Excluir_Sucesso()
+        {
+        }
+    }
+}
---
+++ Projeto_NFe/Projeto_NFe.Infrastructure.Data/Funcionalidades/Nota Fiscal/NotaFiscalRepositorioSql.cs
@@ -0,0 +1,37 @@
+﻿using Projeto_NFe.Domain.Funcionalidades.Nota_Fiscal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_NFe.Infrastructure.Data.Funcionalidades.Nota_Fiscal
+{
+    public class NotaFiscalRepositorioSql : INotaFiscalRepositorio
+    {
+        public NotaFiscal Adicionar(NotaFiscal entidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public NotaFiscal Atualizar(NotaFiscal entidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public NotaFiscal BuscarPorId(long Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<NotaFiscal> BuscarTodos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Excluir(NotaFiscal entidade)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
