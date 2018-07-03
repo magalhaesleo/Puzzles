8acbedca383d31bdb472b32ed011f814f3ddbdfd
William Passig <william.passig@nddigital.com.br>
2018-07-03 17:34:40 -0300

    Feito o projeto em branco

---
+++ projeto-pizzaria/projeto-pizzaria.Applications.Tests/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projeto_pizzaria.Applications.Tests
+{
+    public class Class1
+    {
+    }
+}
