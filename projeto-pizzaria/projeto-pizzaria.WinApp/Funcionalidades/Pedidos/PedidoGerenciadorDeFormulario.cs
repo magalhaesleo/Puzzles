30d8664be11277214214364eb4d67ead0f64bb52
Ana <joao.azevedo@nddigital.com.br>
2018-07-04 16:38:38 -0300

    Foi criado a classe Pedido GerenciadorDeFormulario

---
+++ projeto-pizzaria/projeto-pizzaria.WinApp/Funcionalidades/Pedidos/PedidoGerenciadorDeFormulario.cs
@@ -0,0 +1,41 @@
+﻿using projeto_pizzaria.WinApp.Base;
+using projeto_pizzaria.WinApp.Funcionalidades.Pedidos.RealizarPedido;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace projeto_pizzaria.WinApp.Funcionalidades.Pedidos
+{
+    public class PedidoGerenciadorDeFormulario : GerenciadorFormulario
+    {
+        public override void Adicionar()
+        {
+            RealizarPedido_Dialog dialogRealizarPedido = new RealizarPedido_Dialog();
+
+            DialogResult resultadoDialogRealizarPedido = dialogRealizarPedido.ShowDialog();
+        }
+
+        public override void AtualizarListagem()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Editar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Excluir()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ObtemTipo()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
