7e53b61a6c80c7795dc3803fdce61fd6dabc88c9
magalhaesleo <leonardomagalhaes.leo@gmail.com>
2018-07-05 16:02:07 -0300

    Implementado a classe Produto Pedido Juntamente do objectmother e teste de validação

---
+++ projeto-pizzaria/projeto-pizzaria.Domain.Tests/Funcionalidades/ProdutosPedido/ProdutoPedidoTeste.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using projeto_pizzaria.Domain.Excecoes;
+using projeto_pizzaria.Domain.Funcionalidades.ProdutosPedido;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projeto_pizzaria.Domain.Tests.Funcionalidades.ProdutosPedido
+{
+    [TestFixture]
+    public class ProdutoPedidoTeste
+    {
+        public ProdutoPedido _produtoPedido;
+        [SetUp]
+        public void IniciarCenario()
+        {
+            _produtoPedido = new ProdutoPedido();
+        }
+        [Test]
+        public void ProdutoPedido_Dominio_Validar_Sucesso()
+        {
+            Action resultado =_produtoPedido.Validar;
+
+            resultado.Should().NotThrow<ExcecaoDeNegocio>();
+        }
+        [Test]
+        public void ProdutoPedido_Dominio_Validar_SemProduto_Falha()
+        {
+            Action resultado = _produtoPedido.Validar;
+
+            resultado.Should().NotThrow<ExcecaoDeNegocio>();
+        }
+    }
+}
---
+++ projeto-pizzaria/projeto-pizzaria.Domain/Funcionalidades/ProdutosPedido/ProdutoPedido.cs
@@ -0,0 +1,28 @@
+﻿using projeto_pizzaria.Domain.Base;
+using projeto_pizzaria.Domain.Funcionalidades.Adicionais;
+using projeto_pizzaria.Domain.Funcionalidades.Pedidos;
+using projeto_pizzaria.Domain.Funcionalidades.Produtos;
+using projeto_pizzaria.Domain.Funcionalidades.Sabores;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projeto_pizzaria.Domain.Funcionalidades.ProdutosPedido
+{
+    public class ProdutoPedido : Entidade
+    {
+        public Produto Produto { get; set; }
+        public Pedido Pedido { get; set; }
+        public List<Sabor> Sabores { get; set; }
+
+        public int Quantidade { get; set; }
+        public double ValorTotal { get; set; }
+
+        public void Validar()
+        {
+
+        }
+    }
+}
