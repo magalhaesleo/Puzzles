ba4227fe206d07e02e0c9826c467a5815dd69157
Ana <joao.azevedo@nddigital.com.br>
2018-07-05 16:37:08 -0300

    Adicionando atributos na classe cliente

--- projeto-pizzaria/projeto-pizzaria.Domain/Funcionalidades/Clientes/Cliente.cs
+++ projeto-pizzaria/projeto-pizzaria.Domain/Funcionalidades/Clientes/Cliente.cs
@@ -1,4 +1,5 @@
 ﻿using projeto_pizzaria.Domain.Base;
+using projeto_pizzaria.Domain.Funcionalidades.Enderecos;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,11 @@ namespace projeto_pizzaria.Domain.Funcionalidades.Clientes
 {
     public class Cliente : Entidade
     {
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public Endereco Endereco { get; set; }
+        public IDocumento Documento { get; set; }
         public void Validar()
         {
         }
