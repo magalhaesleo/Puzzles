c2337cb4804b942c6bf439793563bd9726e53a32
Leonardo Magalhães <leonardo.magalhaes@ndd.com.br>
2018-07-09 15:10:48 -0300

    Criado a configuração de calzone

--- projeto-pizzaria/projeto-pizzaria.Domain/Funcionalidades/Produtos/Calzones/Calzone.cs
+++ projeto-pizzaria/projeto-pizzaria.Domain/Funcionalidades/Produtos/Calzones/Calzone.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using projeto_pizzaria.Domain.Funcionalidades.Sabores;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,7 @@ namespace projeto_pizzaria.Domain.Funcionalidades.Produtos.Calzones
 {
     public class Calzone : Produto
     {
+        public Sabor Sabor { get; set; }
         public override string ObterTipo()
         {
             return "Calzone";
---
+++ projeto-pizzaria/projeto-pizzaria.Infra.Data/Funcionalidades/Produtos/Calzones/CalzoneConfiguracao.cs
@@ -0,0 +1,35 @@
+﻿using projeto_pizzaria.Domain.Funcionalidades.Produtos.Calzones;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projeto_pizzaria.Infra.Data.Funcionalidades.Produtos.Calzones
+{
+    public class CalzoneConfiguracao : EntityTypeConfiguration<Calzone>
+    {
+        public CalzoneConfiguracao()
+        {
+            ToTable("TBProdutoPedido");
+
+            HasKey(p => p.Id);
+            HasRequired(p => p.Pedido);
+            HasRequired(p => p.Sabor);
+            Property(p => p.Valor)
+                .HasColumnName("ValorSabor")
+                .IsOptional();
+            Property(p => p.Quantidade)
+                .HasColumnName("Quantidade")
+                .IsRequired();
+            Property(p => p.Valor)
+                .HasColumnName("ValorTotal")
+                .IsRequired();
+            Property(p => p.ObterTipo())
+            .HasColumnName("TipoProduto")
+            .HasColumnType("varchar")
+            .HasMaxLength(50);
+        }
+    }
+}
