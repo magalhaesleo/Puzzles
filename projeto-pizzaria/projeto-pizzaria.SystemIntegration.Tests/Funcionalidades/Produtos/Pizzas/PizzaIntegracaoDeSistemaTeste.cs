1c891b9e2ec610b8720e1f724d32cdb84044de94
William Passig <william.passig@nddigital.com.br>
2018-07-10 17:11:57 -0300

    Feitos os testes de integração de sistema de Pizza. Eles se referiam aos casos de teste levantados

--- projeto-pizzaria/projeto-pizzaria.SystemIntegration.Tests/Funcionalidades/Produtos/Pizzas/PizzaIntegracaoDeSistemaTeste.cs
+++ projeto-pizzaria/projeto-pizzaria.SystemIntegration.Tests/Funcionalidades/Produtos/Pizzas/PizzaIntegracaoDeSistemaTeste.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using FluentAssertions;
+using NUnit.Framework;
 using projeto_pizzaria.Common.Tests;
 using projeto_pizzaria.Domain.Funcionalidades.Adicionais;
 using projeto_pizzaria.Domain.Funcionalidades.Produtos.Pizzas;
@@ -70,91 +71,166 @@ namespace projeto_pizzaria.SystemIntegration.Tests.Funcionalidades.Produtos.Pizz
         [Test]
         public void Pizza_IntegracaoDeSistema_Valor_PizzaPequena_Sucesso()
         {
+            _pizza.Tamanho = TamanhoPizza.PEQUENA;
+            _pizza.Sabor1 = _saborQueijo;
 
+            _pizza.Valor.Should().Be(_saborQueijo.ValorPequena);
         }
 
         [Test]
         public void Pizza_IntegracaoDeSistema_Valor_PizzaPequenaComBordaCatupiry_Sucesso()
         {
+            _pizza.Tamanho = TamanhoPizza.PEQUENA;
+            _pizza.Sabor1 = _saborQueijo;
+            _pizza.Adicional = _bordaCatupiry;
 
+            _pizza.Valor.Should().Be(_saborQueijo.ValorPequena + _bordaCatupiry.ValorPequena);
         }
 
         [Test]
         public void Pizza_IntegracaoDeSistema_Valor_PizzaPequenaComBordaCheddar_Sucesso()
         {
+            _pizza.Tamanho = TamanhoPizza.PEQUENA;
+            _pizza.Sabor1 = _saborQueijo;
+            _pizza.Adicional = _bordaCheddar;
 
+            _pizza.Valor.Should().Be(_saborQueijo.ValorPequena + _bordaCheddar.ValorPequena);
         }
 
         [Test]
         public void Pizza_IntegracaoDeSistema_Valor_PizzaMedia_Sucesso()
         {
+            _pizza.Tamanho = TamanhoPizza.MEDIA;
+            _pizza.Sabor1 = _saborQueijo;
 
+            _pizza.Valor.Should().Be(_saborQueijo.ValorMedia);
         }
 
         [Test]
         public void Pizza_IntegracaoDeSistema_Valor_PizzaMediaComBordaCatupiry_Sucesso()
         {
+            _pizza.Tamanho = TamanhoPizza.MEDIA;
+            _pizza.Sabor1 = _saborQueijo;
+            _pizza.Adicional = _bordaCatupiry;
 
+            _pizza.Valor.Should().Be(_saborQueijo.ValorMedia + _bordaCatupiry.ValorMedia);
         }
 
         [Test]
         public void Pizza_IntegracaoDeSistema_Valor_PizzaMediaComBordaCheddar_Sucesso()
         {
+            _pizza.Tamanho = TamanhoPizza.MEDIA;
+            _pizza.Sabor1 = _saborQueijo;
+            _pizza.Adicional = _bordaCheddar;
 
+            _pizza.Valor.Should().Be(_saborQueijo.ValorMedia + _bordaCheddar.ValorMedia);
         }
 
         [Test]
         public void Pizza_IntegracaoDeSistema_Valor_PizzaGrande_Sucesso()
         {
+            _pizza.Tamanho = TamanhoPizza.GRANDE;
+            _pizza.Sabor1 = _saborQueijo;
 
+            _pizza.Valor.Should().Be(_saborQueijo.ValorGrande);
         }
 
         [Test]
         public void Pizza_IntegracaoDeSistema_Valor_PizzaGrandeComBordaCatupiry_Sucesso()
         {
+            _pizza.Tamanho = TamanhoPizza.GRANDE;
+            _pizza.Sabor1 = _saborQueijo;
+            _pizza.Adicional = _bordaCatupiry;
 
+            _pizza.Valor.Should().Be(_saborQueijo.ValorGrande + _bordaCatupiry.ValorGrande);
         }
 
         [Test]
         public void Pizza_IntegracaoDeSistema_Valor_PizzaGrandeComBordaCheddar_Sucesso()
         {
+            _pizza.Tamanho = TamanhoPizza.GRANDE;
+            _pizza.Sabor1 = _saborQueijo;
+            _pizza.Adicional = _bordaCheddar;
 
+            _pizza.Valor.Should().Be(_saborQueijo.ValorGrande + _bordaCheddar.ValorGrande);
         }
 
         [Test]
         public void Pizza_IntegracaoDeSistema_Valor_PizzaPequenaComDoisSaboresValorIgual_Sucesso()
         {
+            _pizza.Tamanho = TamanhoPizza.PEQUENA;
+            _pizza.Sabor1 = _saborQueijo;
+            _pizza.Sabor2 = _saborCalabresa;
 
+            //Dois sabores com o mesmo preço
+            double precoDosDoisSabores = _saborQueijo.ValorPequena;
+
+            _pizza.Valor.Should().Be(precoDosDoisSabores);
         }
 
         [Test]
         public void Pizza_IntegracaoDeSistema_Valor_PizzaPequenaComDoisSaboresValoresDiferentes_Sucesso()
         {
+            _pizza.Tamanho = TamanhoPizza.PEQUENA;
+            _pizza.Sabor1 = _saborQueijo;
+            _pizza.Sabor2 = _saborCoracao;
 
+            //Sabores com preços diferentes
+            double precoDoSaborMaisCaro = _saborCoracao.ValorPequena;
+
+            _pizza.Valor.Should().Be(precoDoSaborMaisCaro);
         }
 
         [Test]
         public void Pizza_IntegracaoDeSistema_Valor_PizzaMediaComDoisSaboresValorIgual_Sucesso()
         {
+            _pizza.Tamanho = TamanhoPizza.MEDIA;
+            _pizza.Sabor1 = _saborQueijo;
+            _pizza.Sabor2 = _saborCalabresa;
 
+            //Dois sabores com o mesmo preço
+            double precoDosDoisSabores = _saborQueijo.ValorMedia;
+
+            _pizza.Valor.Should().Be(precoDosDoisSabores);
         }
 
         [Test]
         public void Pizza_IntegracaoDeSistema_Valor_PizzaMediaComDoisSaboresValoresDiferentes_Sucesso()
         {
+            _pizza.Tamanho = TamanhoPizza.MEDIA;
+            _pizza.Sabor1 = _saborQueijo;
+            _pizza.Sabor2 = _saborCoracao;
 
+            //Sabores com preços diferentes
+            double precoDoSaborMaisCaro = _saborCoracao.ValorMedia;
+
+            _pizza.Valor.Should().Be(precoDoSaborMaisCaro);
         }
 
         [Test]
         public void Pizza_IntegracaoDeSistema_Valor_PizzaGrandeComDoisSaboresValorIgual_Sucesso()
         {
+            _pizza.Tamanho = TamanhoPizza.GRANDE;
+            _pizza.Sabor1 = _saborQueijo;
+            _pizza.Sabor2 = _saborCalabresa;
 
+            //Dois sabores com o mesmo preço
+            double precoDosDoisSabores = _saborQueijo.ValorGrande;
+
+            _pizza.Valor.Should().Be(precoDosDoisSabores);
         }
 
         [Test]
         public void Pizza_IntegracaoDeSistema_Valor_PizzaGrandeComDoisSaboresValoresDiferentes_Sucesso()
         {
+            _pizza.Tamanho = TamanhoPizza.GRANDE;
+            _pizza.Sabor1 = _saborQueijo;
+            _pizza.Sabor2 = _saborCoracao;
 
+            //Sabores com preços diferentes
+            double precoDoSaborMaisCaro = _saborCoracao.ValorGrande;
+
+            _pizza.Valor.Should().Be(precoDoSaborMaisCaro);
         }
     }
 }
