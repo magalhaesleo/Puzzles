1aa46ed7be6eadaec09f81f405a17f7ec64219cd
William Passig <william.passig@nddigital.com.br>
2018-07-11 17:29:25 -0300

    Adicionado repositório da entidade Adicional apenas com a função de BuscarTodos

---
+++ projeto-pizzaria/projeto-pizzaria.Domain/Interfaces/Adicionais/IAdicionalRepositorio.cs
@@ -0,0 +1,13 @@
+﻿using projeto_pizzaria.Domain.Funcionalidades.Adicionais;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projeto_pizzaria.Domain.Interfaces.Adicionais
+{
+    public interface IAdicionalRepositorio : IRepositorio<Adicional>
+    {
+    }
+}
