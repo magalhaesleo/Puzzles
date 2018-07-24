6657ae540c3d1749b88ebfa9e4a3bdf674d000bf
Leonardo Magalhães Andrade <leonardo.magalhaes@ndd.com.br>
2018-07-24 17:31:58 -0300

    Adicionado teste de saldototal na conta

---
+++ ws-banco-tabajara/ws-banco-tabajara.Application/Funcionalidades/Contas/IContaServico.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ws_banco_tabajara.Application.Funcionalidades.Contas
+{
+    public interface IContaServico
+    {
+    }
+}
---
+++ ws-banco-tabajara/ws-banco-tabajara.Domain.Tests/Funcionalidades/Contas/ContaTeste.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ws_banco_tabajara.Common.Tests.Funcionalidades;
+using ws_banco_tabajara.Domain.Funcionalidades.Contas;
+
+namespace ws_banco_tabajara.Domain.Tests.Funcionalidades.Contas
+{
+    [TestFixture]
+    public class ContaTeste
+    {
+        [Test]
+        public void Conta_Dominio_CalcularSaldoTotal_Sucesso()
+        {
+            Conta conta = ObjectMother.ObterContaValida();
+
+            int valorSaldoTotalObjectMother = 1010;
+
+            conta.SaldoTotal.Should().Be(valorSaldoTotalObjectMother);
+        }
+    }
+}
