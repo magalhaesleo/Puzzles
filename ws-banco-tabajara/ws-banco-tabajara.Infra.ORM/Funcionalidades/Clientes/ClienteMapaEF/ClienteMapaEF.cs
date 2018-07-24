6ebced43a93a6083f8223d5cbf602d6adf194342
unknown <ndduser@ndd-note-ace910.nddigital.local>
2018-07-24 16:19:22 -0300

    Criado o mapa para da entidade cliente para o framework Entity

---
+++ ws-banco-tabajara/ws-banco-tabajara.Infra.ORM/Funcionalidades/Clientes/ClienteMapaEF/ClienteMapaEF.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ws_banco_tabajara.Domain.Funcionalidades.Clientes;
+
+namespace ws_banco_tabajara.Infra.ORM.Funcionalidades.Clientes.ClienteMapaEF
+{
+    public class ClienteMapaEF : EntityTypeConfiguration<Cliente>
+    {
+        public ClienteMapaEF()
+        {
+            ToTable("TBCLIENTE");
+
+            HasKey(cliente => cliente.Id);
+
+            Property(cliente => cliente.DataNascimento).HasColumnName("DATANASCIMENTO");
+
+            Property(cliente => cliente.RG).HasColumnName("RG");
+
+            Property(cliente => cliente.Nome).HasColumnName("NOME").IsRequired();
+
+            
+        }
+    }
+}
