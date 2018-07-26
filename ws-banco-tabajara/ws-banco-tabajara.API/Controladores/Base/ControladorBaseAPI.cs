6f10fd928a10eaf41c9b4774bec7a799d7ac2314
William Passig <william.passig@nddigital.com.br>
2018-07-26 13:57:23 -0300

    Terminadas as configurações gerais com exceção dos csvHandlers

---
+++ ws-banco-tabajara/ws-banco-tabajara.API/Controladores/Base/ControladorBaseAPI.cs
@@ -0,0 +1,90 @@
+﻿using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Web;
+using System.Web.Http;
+using ws_banco_tabajara.API.Excecoes;
+using ws_banco_tabajara.API.Modelos;
+using ws_banco_tabajara.Domain.Excecoes;
+
+namespace ws_banco_tabajara.API.Controladores.Base
+{
+    public class ControladorBaseAPI : ApiController
+    {
+        #region Handlers
+
+        /// <summary>
+        /// Manuseia o callback. Valida se é necessário retornar erro ou o próprio TSuccess
+        /// </summary> 
+        /// <typeparam name="TSuccess"></typeparam>
+        /// <param name="func">É a função que irá retornar o valor para o payload</param>
+        /// <returns></returns>
+        protected IHttpActionResult HandleCallback<TSuccess>(Func<TSuccess> func)
+        {
+            try
+            {
+                return Ok(func());
+            }
+            catch (Exception e)
+            {
+                return HandleFailure(e);
+            }
+        }
+
+        /// <summary>
+        /// Manuseia a query para aplicar as opções do odata.
+        ///
+        /// Esse método vai montar a resposta HTTP solicitada, conforme headers.
+        /// 
+        /// </summary>
+        /// <typeparam name="TResult">Tipo de retorno </typeparam>
+        /// <param name="query">IQueryable(TQueryOptions)</param>
+        /// <returns>IHttpActionResult(TResult) com o resultado da operação</returns>
+        protected IHttpActionResult HandleQuery<TResult>(IQueryable<TResult> query)
+        {
+            if (Request.Headers.Accept.Contains(MediaTypeWithQualityHeaderValue.Parse(TiposDeMidia.Csv)))
+                return ResponseMessage(HandleCSVFile(query));
+
+            return Ok(query.ToList());
+        }
+
+        protected IHttpActionResult HandleQueryable<TSource>(IQueryable<TSource> query)
+        {
+            if (Request.Headers.Accept.Contains(MediaTypeWithQualityHeaderValue.Parse(MediaTypes.Csv)))
+                return ResponseMessage(HandleCSVFile(query));
+
+            return Ok(query.ToList());
+        }
+
+        /// <summary>
+        /// Verifica a exceção passada por parametro para passar o StatusCode correto para o frontend.
+        /// </summary>
+        /// <typeparam name="T">Qualquer classe que herde de Exeption</typeparam>
+        /// <param name="exceptionToHandle">obj de exceção</param>
+        /// <returns></returns>
+        protected IHttpActionResult HandleFailure<T>(T exceptionToHandle) where T : Exception
+        {
+            var exceptionPayload = ExceptionPayload.New(exceptionToHandle);
+            return exceptionToHandle is ExcecaoDeNegocio ?
+                Content(HttpStatusCode.BadRequest, exceptionPayload) :
+                Content(HttpStatusCode.InternalServerError, exceptionPayload);
+        }
+
+        /// <summary>
+        /// Retorna IHttpStatusCode de erro + erros da validação.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="validationFailure">Erros de validação (ValidationFailure)</param>
+        /// <returns>IHttpActionResult com os erros e status code padrão</returns>
+        protected IHttpActionResult HandleValidationFailure<T>(IList<T> validationFailure) where T : ValidationFailure
+        {
+            return Content(HttpStatusCode.BadRequest, validationFailure);
+        }
+
+        #endregion
+
+    }
+}
