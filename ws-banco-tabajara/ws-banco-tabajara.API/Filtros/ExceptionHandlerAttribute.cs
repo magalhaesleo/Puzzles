e4ccd75defe94d79e48ce0e777fbc47e04905ee4
William Passig <william.passig@nddigital.com.br>
2018-07-26 16:03:28 -0300

    Adicionado comentário na classe de filtro para ter maior clareza

--- ws-banco-tabajara/ws-banco-tabajara.API/Filtros/ExceptionHandlerAttribute.cs
+++ ws-banco-tabajara/ws-banco-tabajara.API/Filtros/ExceptionHandlerAttribute.cs
@@ -7,6 +7,19 @@ using ws_banco_tabajara.API.Extensoes;
 
 namespace ws_banco_tabajara.API.Filtros
 {
+    /// <summary>
+    /// Classe que será o atributo nos controllers [ExceptionHandler] que captura as exceções
+    /// e utiliza o extension method HandleExecutedContextException
+    /// para responder ao client com o código e a mensagem adequados.
+    /// 
+    /// Precisamos fazer isso pois temos exceções de negócio, 
+    /// que são definidas em Prova1.Domain 
+    /// e precisa ser montado uma resposta http adequada para o client.
+    /// 
+    /// Para evitar a repetição de [ExceptionHandler] nos controllers,
+    /// foi adicionado globalmente nas configurações em WebApiConfig.Register().
+    /// 
+    /// </summary>
     public class ExceptionHandlerAttribute : ExceptionFilterAttribute
     {
         /// <summary>
