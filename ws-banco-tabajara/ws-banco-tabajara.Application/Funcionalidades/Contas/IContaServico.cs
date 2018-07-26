424bef10c817a02d9d4cb20984d4334a8765e420
joao.azevedo <joao.azevedo@nddigital.com.br>
2018-07-26 15:09:17 -0300

    Foram finalizados os testes de serviço relacionados a entidade Conta

--- ws-banco-tabajara/ws-banco-tabajara.Application/Funcionalidades/Contas/IContaServico.cs
+++ ws-banco-tabajara/ws-banco-tabajara.Application/Funcionalidades/Contas/IContaServico.cs
@@ -10,6 +10,6 @@ namespace ws_banco_tabajara.Application.Funcionalidades.Contas
 {
     public interface IContaServico : IServico<Conta>
     {
-        void AlterarStatusConta(bool status);
+        void AlterarStatusConta(long idConta);
     }
 }
