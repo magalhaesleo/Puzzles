00af0f5a9c1e4463bdc6a9ad1bc1f5635e2f5850
Leonardo Magalhães Andrade <leonardo.magalhaes@ndd.com.br>
2018-07-26 16:32:12 -0300

    Feito o merge e corrigido o objctmother

--- ws-banco-tabajara/ws-banco-tabajara.Common.Tests/Funcionalidades/Movimentacoes/ObjectMother.cs
+++ ws-banco-tabajara/ws-banco-tabajara.Common.Tests/Funcionalidades/Movimentacoes/ObjectMother.cs
@@ -27,7 +27,6 @@ namespace ws_banco_tabajara.Common.Tests.Funcionalidades
             return new Movimentacao()
             {
                 Data = DateTime.Now,
-                Descricao = "Descricao",
                 Valor = 4.50,
                 TipoOperacao = TipoOperacaoMovimentacao.TRANSFERENCIA_RECEBIDA,
                 Conta = conta,
